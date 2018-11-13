7acc34dc2fe3b70d4985271721122a192097dbe5
Inti Pedroso <intipedroso@gmail.com>
2018-11-13 15:31:55 -0300

    added initial documentation version

--- analysis.R
+++ analysis.R
@@ -1,7 +1,8 @@
+library(brms)
 library(VGAM)
 library(plyr)
 library(ggplot2)
-
+library(lmerTest)
 info = read.delim("SraRunTable.txt")
 
 data = read.delim("test_all_10K",header=T)
@@ -15,3 +16,10 @@ s = subset(data, name =="LOC100650155")
 s2 = merge(s[,c("bam","pASE","x","n")],info[,c("Run","treatment","colony_of_origin")],by.x="bam",by.y="Run")
 
 r = lmer(pASE ~ 0 + (1|colony_of_origin) + (1|treatment),data=s2)
+
+f = brm(formula = pASE ~ 0 + (1|colony_of_origin) + (1|treatment),data=s2, family="binomial",sample_prior = TRUE)
+
+
+h <- paste("sd_colony_of_origin__Intercept^2 / (sd_colony_of_origin__Intercept^2 +",
+                       "sd_treatment__Intercept^2 + sd^2) = 0")
+(hyp2 <- hypothesis(f, h, class = NULL))
