5e48b7ab3f47f2d18cc1743c1814a3008534b7ba
Inti Pedroso <intipedroso@gmail.com>
2018-12-03 12:14:05 -0300

    improved speed and some accuracy on calculation comparison of distributions

--- ASE/stats.py
+++ ASE/stats.py
@@ -7,23 +7,51 @@ Created on Tue Oct  2 16:54:06 2018
 """
 
 from scipy.special import betaln as lbeta
+from scipy import logaddexp
 import numpy as np
 from utils import exp_
 
-def prob_2_beats_1_parameters(alpha_1, beta_1, alpha_2, beta_2):
-    total = 0.0
-    for i in np.arange(0,alpha_2-1):
-        total += exp_( lbeta(alpha_1+i,beta_1 + beta_2) - np.log(beta_2+i) - lbeta(1+i,beta_2) - lbeta(alpha_1,beta_1))
+def prob_2_beats_1_binary(alpha_1, beta_1, alpha_2, beta_2, invert_to_seep_up=True):
+    if invert_to_seep_up and ( alpha_2 > alpha_1):
+        alpha_1, alpha_2 = (alpha_2,alpha_1)
+        beta_1, beta_2 = (beta_2,beta_1)
+        
+    total = list()
+    for i in xrange(0,int(np.around(alpha_2-1))):
+        total.append( lbeta(alpha_1+i,beta_1 + beta_2) - np.log(beta_2+i) - lbeta(1+i,beta_2) - lbeta(alpha_1,beta_1))
+    total = exp_(logaddexp.reduce(total))
     return total
 
-def prob_1_diff_2(alpha_1, beta_1, alpha_2, beta_2):
-    p = prob_2_beats_1_parameters(alpha_1, beta_1, alpha_2, beta_2)
+def prob_2_beats_1_counts(alpha_1, beta_1, alpha_2, beta_2, invert_to_seep_up=True):
+    if invert_to_seep_up and (alpha_2 < alpha_1):
+        alpha_1, alpha_2 = (alpha_2,alpha_1)
+        beta_1, beta_2 = (beta_2,beta_1)
+   
+    total = list()
+    for k in xrange(0,int(np.around(alpha_1-1))):
+        total.append(k*np.log(beta_1) + alpha_2*np.log(beta_2) - (k+alpha_2)*np.log(beta_1 + beta_2) - np.log(k+alpha_2) - lbeta(k+1,alpha_2))
+    print total
+    total = exp_(logaddexp.reduce(total))
+    return total
+
+def prob_1_diff_2(alpha_1, beta_1, alpha_2, beta_2, p_type="counts"):
+    p = np.nan
+    
+    if p_type == "counts":
+        p = prob_2_beats_1_counts(alpha_1, beta_1, alpha_2, beta_2)
+    elif p_type == "binary":
+        p = prob_2_beats_1_binary(alpha_1, beta_1, alpha_2, beta_2)
+    
+    else:
+        print "Only probability types for binary and counts are allowed"
     return np.abs(p - (1-p))
 
-def prob_ASE_mixt_prior(alpha,beta,pars):
+def prob_ASE_mixt_prior(alpha,beta,pars, null_pars=None, prior_w = None):
     local_K = pars.shape[0]
-    null_pars = pars[(local_K-1)/2,:]
-    prior_w = 1.0 - np.array([ prob_1_diff_2(pars[i,0],pars[i,1],null_pars[0],null_pars[1]) for i in xrange((local_K -1)/2 + 1)])
-    prob_ASE = np.array([ prob_1_diff_2(alpha,beta,pars[i,0],pars[i,1]) for i in xrange((local_K -1)/2 + 1)])
+    if not null_pars:
+        null_pars = pars[(local_K-1)/2,:]
+    if not prior_w:
+        prior_w = 1.0 - np.array([ prob_1_diff_2(pars[i,0],pars[i,1],null_pars[0],null_pars[1]) for i in xrange(local_K)])
+    prob_ASE = np.array([ prob_1_diff_2(alpha,beta,pars[i,0],pars[i,1]) for i in xrange(local_K)])
     mixt_prob_ASE = np.dot(prob_ASE, prior_w/prior_w.sum())
     return mixt_prob_ASE
