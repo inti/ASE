82c7b5114c35a362c1af1782e025ac3585a9124f
Inti Pedroso <intipedroso@gmail.com>
2019-01-28 16:39:41 -0300

    adding options, seed and scale

--- make_random.py
+++ make_random.py
@@ -1,27 +1,43 @@
 import pandas as pd
 import numpy as np
 import sys
+import argparse
+import logging
 
-print("Arguments: ")
-print("Binomial_Probability:" + sys.argv[1])
-print("filein: " + sys.argv[2])
-print("fileout: " + sys.argv[3])
-print("")
+parser = argparse.ArgumentParser(description='Estimate Allelic Specific Expression probabilities')
+parser.add_argument('--input', type=str, default=None, help='Input file to consider')
+parser.add_argument('--output', type=str, default=None, help='Input files to consider')
+parser.add_argument('--scale', type=int, default=1, help='Multiply readth length by this')
+parser.add_argument('--p', type=float, default=0.5, help='Proportion for null')
+parser.add_argument('--seed', type=int, default=1, help='Seed for random number generator')
+args = parser.parse_args()
 
-data = pd.read_table(sys.argv[2])
 
-print("Data IN")
-print(data.head())
-print("")
+np.random.seed(args.seed)
 
-data.loc[:,"aCount"] = np.random.binomial(n=data.totalCount.values,p=float(sys.argv[1]))
+logger = logging.getLogger()
+handler = logging.StreamHandler()
+formatter = logging.Formatter(
+        '%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logging_level = logging.INFO
+logger.setLevel(logging_level)
+
+data = pd.read_table(args.input)
+
+logger.info("Data IN")
+logger.info(data.head())
+
+data.loc[:,"totalCount"] = data.totalCount.values*args.scale
+data.loc[:,"aCount"] = np.random.binomial(n=data.totalCount.values,p=float(args.p))
 data.loc[:,"bCount"] = data.loc[:,"totalCount"] - data.loc[:,"aCount"]
 
-print("Data OUT")
-print(data.head())
-print("")
+logger.info("Data OUT")
+logger.info(data.head())
 
-data.to_csv(sys.argv[3],sep="\t",header=True,index=False)
 
-print("\n\nDone\n\n")
+data.to_csv(args.output,sep="\t",header=True,index=False)
 
+logger.info("\n\nDone\n\n")
+print("hello")
